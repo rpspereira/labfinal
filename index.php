6525964b3356fb45aee491443db3bce4fc779c03
rpspereira <ruipedro.pereira@my.istec.pt>
2025-06-15 13:03:18 +0100

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,5 +0,0 @@
-        
-        <p style="text-align: center; margin-top: 30px; color: #7f8c8d;"><em>✅ Base de dados configurada automaticamente via Terraform!</em></p>
-    </div>
-</body>
-</html>
